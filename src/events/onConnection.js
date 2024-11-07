030dc8150691ffefeef191ed402013e19b9b6f1f
soonode97 <soonode951@gmail.com>
2024-11-07 14:34:14 +0900

    (윤수빈) update: 시퀀스 검증을 위한 로직 구현, 응답패킷 하드코딩된 부분 수정
    1. 패킷 시퀀스 소켓에 할당
    2. 패킷 파싱, 패킹부분에서 시퀀스 입력되도록 구현
    3. 커스텀 에러에 시퀀스 추가
    4. 유저 클래스가 아닌 소켓에 시퀀스 할당하도록 변경
    5. 응답패킷 하드코딩 수정

--- src/events/onConnection.js
+++ src/events/onConnection.js
@@ -12,6 +12,9 @@ export const onConnection = (socket) => {
 
   // 각 클라이언트마다 고유의 버퍼를 유지하기 위해 빈 버퍼 생성
   socket.buffer = Buffer.alloc(0);
+  // 각 클라이언트의 패킷순서를 보장하기 위한 순서 생성
+  socket.sequence = 0;
+  console.log(socket.sequence);
 
   socket.on('data', onData(socket));
   socket.on('end', onEnd(socket));
